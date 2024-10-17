13a8956dabf7f3c7158f26bf73e2b9b46ce8952e
Hamza Malik <muhammadhamzamlk@gmail.com>
2024-10-17 13:55:27 +0500

    feat: new

--- popup.js
+++ popup.js
@@ -15,65 +15,44 @@ function scrapeData() {
 
     data.forEach(article => {
         let nameElement = article.querySelector('div.x1yztbdb strong');
-        let messageSpan = article.querySelector('div[data-ad-preview="message"] span');
-        let imageDiv = article.querySelector('.x10l6tqk.x17qophe.x13vifvy.xh8yej3 .x1vvkbs');
-        let r13e = article.querySelector('div[id=":r13e:"] span');
-        let r1d6 = article.querySelector('div[id=":r1d6:"] span');
-        let r1ge = article.querySelector('div[id=":r1ge:"] span');
-        let r1h0 = article.querySelector('div[id=":r1h0:"] span');
-        let r1kh = article.querySelector('div[id=":r1kh:"] span');
-        let r1mc = article.querySelector('div[id=":r1mc:"] span');
-        let r1sa = article.querySelector('div[id=":r1sa:"] span');
-        let r20i = article.querySelector('div[id=":r20i:"] span');
-        let r21h = article.querySelector('div[id=":r21h:"] span');
-        let r222 = article.querySelector('div[id=":r222:"] span');
-        let r29v = article.querySelector('div[id=":r29v:"] span');
-        let r2al = article.querySelector('div[id=":r2al:"] span');
-        let r2t7 = article.querySelector('div[id=":r2t7:"] span');
-        let blockquote = article.querySelector('blockquote');
+        let time = article.querySelector('span[dir="ltr"] div span span span a');
 
+        const contentElements = [
+            article.querySelector('div[data-ad-preview="message"] span'),
+            article.querySelector('.x10l6tqk.x17qophe.x13vifvy.xh8yej3 .x1vvkbs'),
+            article.querySelector('div[id=":r13e:"] span'),
+            article.querySelector('div[id=":r1d6:"] span'),
+            article.querySelector('div[id=":r1ge:"] span'),
+            article.querySelector('div[id=":r1h0:"] span'),
+            article.querySelector('div[id=":r1kh:"] span'),
+            article.querySelector('div[id=":r1mc:"] span'),
+            article.querySelector('div[id=":r1sa:"] span'),
+            article.querySelector('div[id=":r20i:"] span'),
+            article.querySelector('div[id=":r21h:"] span'),
+            article.querySelector('div[id=":r222:"] span'),
+            article.querySelector('div[id=":r29v:"] span'),
+            article.querySelector('div[id=":r2al:"] span'),
+            article.querySelector('div[id=":r2t7:"] span'),
+            article.querySelector('div[id=":r43l:"] span'),
+            article.querySelector('div[id=":r3im:"] span'),
+            article.querySelector('blockquote')
+        ];
 
         let name = nameElement ? nameElement.innerText.trim() : "Unknown";
         let description = "";
 
-        if (messageSpan) {
-            description = messageSpan.innerText.trim();
-        } else if (imageDiv) {
-            description = imageDiv.innerText.trim();
-        } else if (r13e) {
-            description = r13e.innerText.trim();
-        } else if (r1d6) {
-            description = r1d6.innerText.trim();
-        } else if (r1ge) {
-            description = r1ge.innerText.trim();
-        } else if (r1h0) {
-            description = r1h0.innerText.trim();
-        } else if (r1kh) {
-            description = r1kh.innerText.trim();
-        } else if (r1mc) {
-            description = r1mc.innerText.trim();
-        } else if (r1sa) {
-            description = r1sa.innerText.trim();
-        } else if (r20i) {
-            description = r20i.innerText.trim();
-        } else if (r21h) {
-            description = r21h.innerText.trim();
-        } else if (r222) {
-            description = r222.innerText.trim();
-        } else if (r29v) {
-            description = r29v.innerText
-        } else if (r2al) {
-            description = r2al.innerText
-        } else if (r2t7) {
-            description = r2t7.innerText
-        } else if (blockquote) {
-            description = blockquote.innerText.trim();
+        for (let element of contentElements) {
+            if (element && element.innerText.trim()) {
+                description = element.innerText.trim();
+                break;
+            }
         }
 
         if (description) {
             scrapedDataArr.push({
                 name: name,
-                post: description
+                post: description,
+                time: time ? time.innerText : "Unknown"
             });
         }
     });
